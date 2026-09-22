76fee6798688ca9b956c044379db7398fb67741e
titus-79 <titus@ik.me>
2026-09-22 10:57:09 +0200

    Récupération des roles

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -37,6 +37,24 @@ export async function login(credentials: LoginCredentials): Promise<LoginRespons
 
 export function logoutToken() {
     removeToken();
-
+}
+function decodeJwtPayload(token: string) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64). split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload)
+}
+export function getRoles(): string [] {
+    const token = getToken();
+    if (!token) {
+        return [];
+    }
+    const paylaod = decodeJwtPayload(token);
+    return paylaod.scope ?? [];
 }
 
+export function hasRole(role: string): boolean {
+    return getRoles().includes(role)
+}
